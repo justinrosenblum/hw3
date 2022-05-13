ca9f29563a611093c5f01b801b2148c2e6eee86d
justinrosenblum <Justin.Rosenblum@kellogg.northwestern.edu>
2022-05-13 19:25:28 +0000

    Began to build out controllers for places and post

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,38 @@
 class PlacesController < ApplicationController
-end
+
+    def index
+        @places = Places.all
+    end
+
+    def show
+        @place = Place.find_by({ "id" => params["id"] })
+        @posts = Post.where({ "place_id" => @place["id"] })
+      end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+    end
+
+    def edit
+        @place = Place.find_by({ "id" => params["id"] })
+      end
+    
+      def update
+        @place = Place.find_by({ "id" => params["id"] })
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+    
+      def destroy
+        @place = Place.find_by({ "id" => params["id"] })
+        @place.destroy
+        redirect_to "/places"
+      end
