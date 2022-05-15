969e728e55f390c05cacbe8c3e31ef898ffff9dc
justinrosenblum <Justin.Rosenblum@kellogg.northwestern.edu>
2022-05-15 23:42:36 +0000

    completed assignment. to check work later.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -20,9 +20,4 @@ class PlacesController < ApplicationController
         redirect_to "/places"
     end
     
-    def destroy
-        @place = Place.find_by({ "id" => params["id"] })
-        @place.destroy
-        redirect_to "/places"
-    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,8 @@
 class PostsController < ApplicationController
-
-    def index
-        @posts = Post.all
-      end
     
       def new
         @post = Post.new
+        @post["place_id"] = params["place_id"]
       end
     
       def create
@@ -15,7 +12,7 @@ class PostsController < ApplicationController
         @post["posted_on"] = params["post"]["posted_on"]
         @post["place_id"] = params["post"]["place_id"]
         @post.save
-        redirect_to "/posts"
+        redirect_to "/places/#{@post["place_id"]}"
       end
 
 end
